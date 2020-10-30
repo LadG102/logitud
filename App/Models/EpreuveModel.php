c05b6c5c2859ad450205b72e0114aabf73bab423
LadG102 <l.gakou1@gmail.com>
2020-10-30 16:14:34 +0100

    essai 2 base de donnée

--- App/Models/EpreuveModel.php
+++ App/Models/EpreuveModel.php
@@ -4,19 +4,23 @@ namespace App\Models;
 
 use App\Model\ConnectionDb;
 
-require '../db/ConnDB.php';
-
 class EpreuveModel extends ConnectionDb
 {
+    public function __construct()
+    {
+        $pdo = new ConnectionDb();
+        $this->sreq = $pdo->connDb();
+    }
+
 
     public function saveEpreuveData($request)
     {
-        $req = $pdo->prepare('INSERT INTO epreuve(id_epreuve, nomEpreuve, lieuEpreuve, dateEpreuve) VALUES(:id, :nom, :lieu, :date)');
+        $req = $pdo->prepare('INSERT INTO epreuve(id_epreuve, nomEpreuve, lieuEpreuve, dateEpreuve) VALUES(:id, :nom, :lieu, :dateEpreuve)');
         $req->execute(array(
             'id' => NULL,
             'nom' => $request->request->get('nomEpreuve'),
             'lieu' => $request->request->get('lieuEpreuve'),
-            'date' => $request->request->get('dateEpreuve')
+            'dateEpreuve' => $request->request->get('dateEpreuve')
 
         ));
 
